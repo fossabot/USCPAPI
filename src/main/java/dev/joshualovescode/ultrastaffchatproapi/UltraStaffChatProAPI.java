fc01db2adca7c4ecf0e3032f67add93c89521969
Joshua Sing <joshua@joshualovescode.dev>
2020-06-18 13:13:42 +1000

    Update UltraStaffChatProAPI.java

--- src/main/java/dev/joshualovescode/ultrastaffchatproapi/UltraStaffChatProAPI.java
+++ src/main/java/dev/joshualovescode/ultrastaffchatproapi/UltraStaffChatProAPI.java
@@ -1,9 +1,19 @@
 /**
- * UltraStaffChatProAPI
- * Copyright (c) 2020 Joshua Sing & Myles Newton.
- * All Rights Reserved.
+ *    UltraStaffChatProAPI (USCPAPI) - The official Develoepr API of UltraStaffChatPro.
+ *    Copyright (C) 2020  Joshua Sing & Myles Newton. <contact@joshualovescode.dev>
  *
- * License: BSD-3-Clause License
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as published
+ *    by the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package dev.joshualovescode.ultrastaffchatproapi;
@@ -19,32 +29,69 @@ public class UltraStaffChatProAPI {
     private static APIHandler handler;
     private static APIReceiver receiver;
 
+    /**
+     * Constructor
+     * USED BY ULTRASTAFFCHATPRO, You do not need to do anything with this!
+     * @param APIHandler - APIHandler
+     */
     public UltraStaffChatProAPI(APIHandler APIHandler){
         handler = APIHandler;
         receiver = new APIReceiver();
     }
 
+    /**
+     * get Handler
+     * USED BY ULTRASTAFFCHATPRO, You do not need to do anything with this!
+     * @return APIHandler
+     */
     public static APIHandler getHandler() { return handler; }
 
+    /**
+     * Get Receiver
+     * USED BY ULTRASTAFFCHATPRO, You do not need to do anything with this!
+     * @return APIReceiver
+     */
     private static APIReceiver getReceiver(){
         return receiver;
     }
 
+    /**
+     * Send Message
+     * @param m - USCPMessage
+     */
     public void sendMessage(USCPMessage m){
         getHandler().sendMessage(m);
     }
 
+    /**
+     * Handle Message
+     * USED BY ULTRASTAFFCHATPRO, You do not need to do anything with this!
+     * @param m - USCPMessage
+     */
     public static void handleMessage(USCPMessage m) { getReceiver().receiveMessage(m); }
 
+    /**
+     * Init
+     * USED BY ULTRASTAFFCHATPRO, You do not need to do anything with this!
+     * @param apiHandler - APIHandler
+     */
     public static void init(APIHandler apiHandler){
         api = new UltraStaffChatProAPI(apiHandler);
         em = new EventManager();
     }
 
+    /**
+     * Get EventManager
+     * @return EventManager
+     */
     public static EventManager getEventManager() {
         return em;
     }
 
+    /**
+     * Get UltraStaffChatProAPI
+     * @return UltraStaffChatProAPI
+     */
     public static UltraStaffChatProAPI getAPI(){
         if(api == null){
             try {
@@ -58,17 +105,5 @@ public class UltraStaffChatProAPI {
         }
         return api;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
 }
